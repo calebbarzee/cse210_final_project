68aa1f5d1bec780f4b7de5201d7240b6435e2b15
calebbarzee <barzeec@gmail.com>
2021-12-12 21:41:22 -0700

    formatting

--- game/services/audio_service.py
+++ game/services/audio_service.py
@@ -2,6 +2,7 @@ import sys
 from game.point import Point
 import raylibpy
 
+
 class AudioService:
     """Handles all the audio in the game.
 
@@ -13,12 +14,12 @@ class AudioService:
 
     def __init__(self):
         """The class constructor.
-        
+
         Args:
             self (InputService): An instance of InputService.
         """
         self._sounds = {}
-        
+
     def play_sound(self, filename):
         """
         Plays the sound file provided. Make sure to call start_audio before this is called.
--- game/services/output_service.py
+++ game/services/output_service.py
@@ -2,6 +2,7 @@ import sys
 from game import constants
 import raylibpy
 
+
 class OutputService:
     """Outputs the game state. The responsibility of the class of objects is to draw the game state on the terminal. 
 
